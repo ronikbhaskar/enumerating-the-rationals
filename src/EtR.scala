7dc3ccd2e398860d284111562ae5b49dc4512ba0
Ronik Bhaskar <ronikbhaskar@Roniks-Laptop.lan>
2022-11-21 15:04:56 -0600

    enumerated the rationals

--- src/EtR.scala
+++ src/EtR.scala
@@ -7,6 +7,18 @@
 
 import scala.io.Source
 
+/* types and constructors */
+
+type Rational = (Int, Int)
+
+def mkRat(num: Int, den: Int): Rational = 
+  if num <= 0 || den <= 0 then throw new Exception(s"mkRat: unable to construct positive rational with $num and $den")
+  else (num, den)
+
+def rational2str(q: Rational): String = s"${q(0)}/${q(1)}"
+
+def rationals2str(qs: List[Rational]): String = qs.map(rational2str).mkString(", ")
+
 /* === 2 Greatest Common Divisor === */
 
 /* gcd - calculates the gcd of two natural numbers using Euclid's Algorithm
@@ -66,4 +78,27 @@ def ungcd(d: Int, bs: List[Boolean]): (Int, Int) =
   }
   bs.foldRight((d, d))(undo)
 
+/* boolseqs - enumerates the first 2^(n+1) - 1 boolean sequences
+ *
+ * n : Int - used to create bound on number of boolean sequences generated
+ *
+ * Returns: List[List[Boolean]] - list of boolean sequences in increasing lexicographic order
+ */
+def boolseqs(n: Int): List[List[Boolean]] = 
+  def bsaccumulate(m: Int, bseqs: List[List[Boolean]]): List[List[Boolean]] = m match {
+    case 1 => bseqs
+    case _ => bsaccumulate(m - 1, bseqs ::: bseqs.map(x => false :: x) ::: bseqs.map(x => true :: x))
+  }
+  if n <= 0 then throw new Exception("boolseqs: n must be positive") 
+  else (Nil) :: bsaccumulate(n, (false :: Nil) :: (true :: Nil) ::Nil)
+  
+/* enumRationals - enumerates the rationals using finite boolean sequences
+ *
+ * n : Int - used to create bound on number of boolean sequences generated
+ * 
+ * Returns: List[Rational] - enumrated list of rationals
+ */
+def enumRationals(n: Int): List[Rational] = boolseqs(n).map(ungcd.curried(1))
+
 /* === 3 The Stern-Brocot Tree === */
+
